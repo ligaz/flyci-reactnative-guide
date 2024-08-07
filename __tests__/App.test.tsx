97a1dcbae3997af1247485b48a3f13275ca463ae
Stefan Dobrev <stefan.dobrev@gmail.com>
2024-08-07 12:00:38 +0300

    Test dev wingman

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,10 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
+import { render, screen } from '@testing-library/react-native';
 import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
-import { it } from '@jest/globals';
+import { it, expect } from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -20,4 +17,12 @@ it('should render content correctly', () => {
   const appContent = renderer.create(<App />).toJSON();
 
   expect(appContent).toMatchSnapshot();
+});
+
+it('should render Step One', async () => {
+  render(<App />);
+
+  const stepOne = await screen.findByText('Step One');
+
+  expect(stepOne).toHaveTextContent('Steps');
 });
